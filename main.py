78c9eb4b803157ba80f3bc125fb197503ea73d9a
morrilet <findwaldoFTLOG@gmail.com>
2020-05-30 15:17:44 -0400

    First pass at cube creation (Catherine)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import random
 
 def fetchCards():
     """
@@ -60,7 +61,7 @@ def getCardsOfRarity(rarity, cards):
     return list(filter(lambda card: card['rarity'] == rarity, cards))
 
 
-def getCardsOfColors(colors, cards):
+def getCardsOfColors(colors, cards, exact=True):
     """
     Get a list of cards with the given colors exactly.
 
@@ -68,8 +69,56 @@ def getCardsOfColors(colors, cards):
     :cards: A list of card objects to search.
     :returns: A list of cards objects with the given color identity.
     """
+    if exact:
+        return list(filter(lambda card: set(card['colors']) == set(colors), cards))
+    else:
+        return list(filter(lambda card: set(card['colors']).issuperset(colors), cards))
 
-    return list(filter(lambda card: set(card['color']).issuperset(colors), cards))
+def createCube(cards):
+    """
+    Creates desired cube from "cards" list.
 
+    :cards: list of cards from which cube is created.
 
+    """
 
+    c = 'common'
+    u = 'uncommon'
+    r = 'rare'
+
+    random.shuffle(cards)
+
+    whiteCards = getCardsOfColors(['W'], cards)
+    blueCards = getCardsOfColors(['U'], cards)
+    blackCards = getCardsOfColors(['B'], cards)
+    redCards = getCardsOfColors(['R'], cards)
+    greenCards = getCardsOfColors(['G'], cards)
+    #multiCards = getCardsOfColors(??, cards)
+    #colorless = getCardsOfColors(??, cards)
+
+    whiteCommons = getCardsOfRarity(c, whiteCards)[:50]
+    blueCommons = getCardsOfRarity(c, blueCards)[:51]
+    blackCommons = getCardsOfRarity(c, blackCards)[:50]
+    redCommons = getCardsOfRarity(c, redCards)[:51]
+    greenCommons = getCardsOfRarity(c, greenCards)[:50]
+    #multiCommons = getCardsOfRarity(c, multiCards)[:32]
+    #colorlessCommons = getCardsOfRarity(c, multiCards)[:32]
+
+    whiteUncommons = getCardsOfRarity(u, whiteCards)[:13]
+    blueUncommons = getCardsOfRarity(u, blueCards)[:12]
+    blackUncommons = getCardsOfRarity(u, blackCards)[:13]
+    redUncommons = getCardsOfRarity(u, redCards)[:13]
+    greenUncommons = getCardsOfRarity(u, greenCards)[:13]
+    # multiUncommons = getCardsOfRarity(u, multiCards)[:8]
+    # colorlessUncommons = getCardsOfRarity(u, multiCards)[:8]
+
+    blueRares = getCardsOfRarity(r, blueCards)[:1]
+    blackRares = getCardsOfRarity(r, blackCards)[:1]
+    redRares = getCardsOfRarity(r, redCards)[:1]
+    greenRares = getCardsOfRarity(r, greenCards)[:1]
+
+    commons = whiteCommons + blueCommons + blackCommons + redCommons + greenCommons  # + multiCommons + colorlessCommons
+    uncommons = whiteUncommons + blueUncommons + blackUncommons + redUncommons + greenUncommons  # + multiUncommons + colorlessUncommons
+    rares = whiteCards + blueRares + blackRares + redRares + greenRares  # + multiRares + colorlessRares
+
+    return commons + uncommons + rares
